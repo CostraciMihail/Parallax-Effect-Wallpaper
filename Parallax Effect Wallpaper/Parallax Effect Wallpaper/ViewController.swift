fe335b4786cef04753b967293f2bcb92f67dc41c
Costraci Mihail <mihaicostraci3@gmail.com>
2016-03-16 12:20:07 +0200

    First commit

---
+++ Parallax Effect Wallpaper/Parallax Effect Wallpaper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Parallax Effect Wallpaper
+//
+//  Created by winify on 3/16/16.
+//  Copyright © 2016 Costraci Mihail. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
